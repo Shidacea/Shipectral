f16827c9bb46dd3e669bb6e71b5da761dc8ba246
Hadeweka <hadeweka@outlook.de>
2020-11-23 17:36:26 +0100

    Simplified color wrapper

--- engine/graphics/Color.cr
+++ engine/graphics/Color.cr
@@ -1,30 +1,12 @@
 module SF
+
+  @[MrbWrap::SpecializeInstanceMethod("initialize", [red : Int, green : Int, blue : Int, alpha : Int = 255])]
+  @[MrbWrap::ExcludeInstanceMethod("inspect")]
   struct Color
-    def sdc_add(other)
-      self + other
-    end
 
-    def sdc_subtract(other)
-      self - other
-    end
-
-    def sdc_multiply(other)
-      self * other
-    end
   end
 end
 
 def setup_ruby_color_class(mrb, module_sdc)
-  MrbWrap.wrap_class(mrb, SF::Color, "Color", under: SF)
-  MrbWrap.wrap_constructor(mrb, SF::Color, [{UInt8, 0}, {UInt8, 0}, {UInt8, 0}, {UInt8, 255}])
-  MrbWrap.wrap_instance_method(mrb, SF::Color, "to_i", to_integer)
-  MrbWrap.wrap_property(mrb, SF::Color, "r", r, UInt8)
-  MrbWrap.wrap_property(mrb, SF::Color, "g", g, UInt8)
-  MrbWrap.wrap_property(mrb, SF::Color, "b", b, UInt8)
-  MrbWrap.wrap_property(mrb, SF::Color, "a", a, UInt8)
-  MrbWrap.wrap_instance_method(mrb, SF::Color, "+", sdc_add, [SF::Color])
-  MrbWrap.wrap_instance_method(mrb, SF::Color, "-", sdc_subtract, [SF::Color])
-  MrbWrap.wrap_instance_method(mrb, SF::Color, "*", sdc_multiply, [SF::Color])
-  MrbWrap.wrap_instance_method(mrb, SF::Color, "to_s", to_s)
-  MrbWrap.wrap_instance_method(mrb, SF::Color, "inspect", inspect)
+  MrbWrap.wrap_class_with_methods(mrb, SF::Color, under: SF, verbose: true)
 end
--- engine/graphics/Text.cr
+++ engine/graphics/Text.cr
@@ -1,3 +1,4 @@
+@[MrbWrap::ExcludeConstant("Vector2")]
 module SF
 
   @[MrbWrap::SpecializeInstanceMethod("initialize", [string : String, font : Font, character_size : Int = 30])]
@@ -25,10 +26,4 @@ end
 
 def setup_ruby_text_class(mrb)
   MrbWrap.wrap_class_with_methods(mrb, SF::Text, under: SF, verbose: true)
-  #MrbWrap.wrap_class(mrb, SF::Text, "Text", under: module_sdc)
-  #MrbWrap.wrap_constructor(mrb, SF::Text, [String, SF::Font, {Int32, 30}])
-  #MrbWrap.wrap_property(mrb, SF::Text, "character_size", character_size, Int32)
-  #MrbWrap.wrap_property(mrb, SF::Text, "fill_color", fill_color, SF::Color)
-  #MrbWrap.wrap_property(mrb, SF::Text, "outline_color", outline_color, SF::Color)
-  #MrbWrap.wrap_property(mrb, SF::Text, "string", string, String) # Might need some rework to ensure full Unicode compatibility
 end
