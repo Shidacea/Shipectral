08422c2054ef15ab1d2cd029ed1c422c9557c18d
Hadeweka <hadeweka@outlook.de>
2021-05-16 15:59:41 +0200

    Simplified Coordinates class

--- engine/internal/Coordinates.cr
+++ engine/internal/Coordinates.cr
@@ -1,20 +1,13 @@
 module SF
   @[Anyolite::SpecifyGenericTypes([T])]
   @[Anyolite::ExcludeInstanceMethod("each")]
-  @[Anyolite::ExcludeInstanceMethod("==")]
-  @[Anyolite::ExcludeInstanceMethod("*")]
+  @[Anyolite::SpecializeInstanceMethod("==", [other : Vector2], [other : Vector2(T)])]
+  @[Anyolite::SpecializeInstanceMethod("*", [n : Number])]
   @[Anyolite::SpecializeInstanceMethod("+", [other], [other : Vector2(T)])]
   @[Anyolite::SpecializeInstanceMethod("-", [other], [other : Vector2(T)])]
   @[Anyolite::SpecializeInstanceMethod("initialize", [x : T, y : T])]
   @[Anyolite::WrapWithoutKeywordsInstanceMethod("initialize")]
   struct Vector2(T)
-
-    @[Anyolite::WrapWithoutKeywords]
-    @[Anyolite::Rename("*")]
-    def multiply(other : T)
-      self * other
-    end
-
     @[Anyolite::WrapWithoutKeywords]
     def dot(other : Vector2(T))
       self * other
@@ -25,6 +18,6 @@ module SF
 end
 
 def setup_ruby_coordinates_class(rb)
-  Anyolite.wrap_class_with_methods(rb, SF::Coordinates, under: SF)
-  Anyolite.wrap_class_with_methods(rb, SF::Vector2i, under: SF)
+  Anyolite.wrap_class_with_methods(rb, SF::Coordinates, under: SF, verbose: true)
+  Anyolite.wrap_class_with_methods(rb, SF::Vector2i, under: SF, verbose: true)
 end
