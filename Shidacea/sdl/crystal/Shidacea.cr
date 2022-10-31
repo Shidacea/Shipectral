f025b9d8e9158ad0a3fb5c1990c0eed6d8ede155
Philipp Schulz <hadeweka@outlook.de>
2022-10-31 22:48:57 +0100

    Rearranged SDC/SDL

--- Shidacea/sdl/crystal/Shidacea.cr
+++ Shidacea/sdl/crystal/Shidacea.cr
@@ -3,33 +3,36 @@ SPT::Features.ensure("sdl-image")
 SPT::Features.ensure("sdl-mixer")
 SPT::Features.ensure("sdl-ttf")
 
-require "./wrappers/Helper.cr"
+require "./wrappers/basic/Helper.cr"
+require "./wrappers/basic/Main.cr"
 
-require "./wrappers/Main.cr"
+require "./wrappers/audio/Music.cr"
+require "./wrappers/audio/Sound.cr"
 
-require "./wrappers/CollisionShapes.cr"
-require "./wrappers/Color.cr"
-require "./wrappers/Coords.cr"
-require "./wrappers/Drawable.cr"
-require "./wrappers/Entity.cr"
-require "./wrappers/Event.cr"
-require "./wrappers/Font.cr"
-require "./wrappers/Keyboard.cr"
-require "./wrappers/Limiter.cr"
-require "./wrappers/Mouse.cr"
-require "./wrappers/Music.cr"
-require "./wrappers/Param.cr"
-require "./wrappers/Rect.cr"
-require "./wrappers/Renderer.cr"
-require "./wrappers/RenderQueue.cr"
-require "./wrappers/Scene.cr"
-require "./wrappers/Shapes.cr"
-require "./wrappers/Sound.cr"
-require "./wrappers/Sprite.cr"
-require "./wrappers/Text.cr"
-require "./wrappers/Texture.cr"
-require "./wrappers/View.cr"
-require "./wrappers/Window.cr"
+require "./wrappers/basic/CollisionShapes.cr"
+require "./wrappers/basic/Coords.cr"
+require "./wrappers/basic/Rect.cr"
+
+require "./wrappers/graphics/Color.cr"
+require "./wrappers/graphics/Drawable.cr"
+require "./wrappers/graphics/Font.cr"
+require "./wrappers/graphics/Renderer.cr"
+require "./wrappers/graphics/RenderQueue.cr"
+require "./wrappers/graphics/Shapes.cr"
+require "./wrappers/graphics/Sprite.cr"
+require "./wrappers/graphics/Text.cr"
+require "./wrappers/graphics/Texture.cr"
+require "./wrappers/graphics/View.cr"
+require "./wrappers/graphics/Window.cr"
+
+require "./wrappers/input/Event.cr"
+require "./wrappers/input/Keyboard.cr"
+require "./wrappers/input/Mouse.cr"
+
+require "./wrappers/user/Entity.cr"
+require "./wrappers/user/Limiter.cr"
+require "./wrappers/user/Param.cr"
+require "./wrappers/user/Scene.cr"
 
 def load_engine_library(rb)
   Anyolite.wrap(rb, SDC, verbose: true)
